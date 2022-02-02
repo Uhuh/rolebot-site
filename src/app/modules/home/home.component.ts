7f1bc6904727160a1f7675cfa91ec5b31a4f836d
Dylan Warren <dylanwarren19@gmail.com>
2022-02-01 23:14:07 -0600

    Instead of using the horrible png for the embed use the custom one

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { IReactRole } from 'src/app/shared/types/interfaces';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  reactRoles: IReactRole[] = [
+    {
+      hexCode: '#dd2e44',
+      name: 'Red',
+      unicode: '🟥',
+    },
+    {
+      hexCode: '#2eb166',
+      name: 'Green',
+      unicode: '🟩',
+    },
+    {
+      hexCode: '#3387c0',
+      name: 'Blue',
+      unicode: '🟦',
+    },
+    {
+      hexCode: '#d6b014',
+      name: 'Yellow',
+      unicode: '🟨',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CategoryCardModule } from 'src/app/shared/components/category-card/category-card.module';
 import { HomeComponent } from './home.component';
 
 @NgModule({
@@ -8,6 +9,7 @@ import { HomeComponent } from './home.component';
   imports: [
     RouterModule.forChild([{ path: '', component: HomeComponent }]),
     RouterModule,
+    CategoryCardModule,
     CommonModule,
   ],
 })
