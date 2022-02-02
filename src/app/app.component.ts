db7c3663e6fd442a54735ec58ed7fb62f5115f15
Dylan Warren <dylanwarren19@gmail.com>
2022-02-01 18:22:23 -0600

    Get rid of repetative CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'app';
-
   onActivate() {
     window.scroll(0, 0);
-    console.log(`DWFERWQ`);
   }
 }
