479a517beea0619932fbf9dbfe0627a766e1c6be
Dylan Warren <dylanwarren19@gmail.com>
2022-02-14 17:15:02 -0600

    BrowserAnimation for Angular animations

--- src/app/shared/components/navbar/navbar.module.ts
+++ src/app/shared/components/navbar/navbar.module.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar.component';
 
 @NgModule({
   declarations: [NavbarComponent],
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule, BrowserAnimationsModule],
   exports: [NavbarComponent],
 })
 export class NavbarModule {}
