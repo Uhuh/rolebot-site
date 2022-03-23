e4524ca030a8b6cb3457b95d32f6c52352aed14f
Dylan Warren <dylanwarren19@gmail.com>
2022-03-23 07:29:10 -0500

    Category store still needs to be impl'd

--- src/app/modules/category-store/category-store.component.ts
+++ src/app/modules/category-store/category-store.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ICategory } from 'src/app/shared/types/interfaces';
 
 @Component({
   selector: 'app-category-store',
@@ -7,35 +6,6 @@ import { ICategory } from 'src/app/shared/types/interfaces';
   styleUrls: ['./category-store.component.scss'],
 })
 export class CategoryStoreComponent implements OnInit {
-  categories: ICategory[] = [
-    {
-      title: 'Colors',
-      description: 'Want to be a specific color?',
-      reactRoles: [
-        {
-          hexCode: '#dd2e44',
-          name: 'Red',
-          unicode: '🟥',
-        },
-        {
-          hexCode: '#2eb166',
-          name: 'Green',
-          unicode: '🟩',
-        },
-        {
-          hexCode: '#3387c0',
-          name: 'Blue',
-          unicode: '🟦',
-        },
-        {
-          hexCode: '#d6b014',
-          name: 'Yellow',
-          unicode: '🟨',
-        },
-      ],
-    },
-  ];
-
   constructor() {}
 
   ngOnInit(): void {}
