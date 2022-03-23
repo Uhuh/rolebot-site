d709f52b709468b814db36a67ec5636d9e77d50b
Dylan Warren <dylanwarren19@gmail.com>
2022-03-23 07:29:25 -0500

    React role portion of category

--- src/app/shared/components/category-card/category-react-role/category-react-role.component.ts
+++ src/app/shared/components/category-card/category-react-role/category-react-role.component.ts
@@ -8,8 +8,21 @@ import { IReactRole } from 'src/app/shared/types/interfaces';
 })
 export class CategoryReactRoleComponent implements OnInit {
   @Input() reactRoles: IReactRole[] = [];
+  readonly discordEmojiUrl = 'https://cdn.discordapp.com/emojis';
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  isCustomEmoji(reactRole: IReactRole) {
+    return /\d{17,19}/.exec(reactRole?.emojiId || '');
+  }
+
+  emoji(reactRole: IReactRole) {
+    const match = /\d{17,19}/.exec(reactRole?.emojiId || '');
+
+    return match
+      ? `${this.discordEmojiUrl}/${reactRole?.emojiId}`
+      : reactRole?.emojiId;
+  }
 }
