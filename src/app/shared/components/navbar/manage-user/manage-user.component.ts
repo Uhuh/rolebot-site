b8f95f3b4ced2902102ffb2be9efd402a316c97a
Dylan Warren <dylanwarren19@gmail.com>
2022-03-23 07:29:54 -0500

    Manager user for nav. We're hiding this for now

---
+++ src/app/shared/components/navbar/manage-user/manage-user.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { combineLatest } from 'rxjs';
+import { AuthenticationService } from 'src/app/shared/services/authentication.service';
+import { JwtService } from 'src/app/shared/services/jwtHandler.service';
+
+@Component({
+  selector: 'app-manage-user',
+  templateUrl: './manage-user.component.html',
+  styleUrls: ['./manage-user.component.scss'],
+})
+export class ManageUserComponent implements OnInit {
+  avatar?: string;
+  userId?: string;
+
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly authService: AuthenticationService
+  ) {}
+
+  ngOnInit(): void {
+    combineLatest([this.jwtService.avatar$, this.jwtService.userId$]).subscribe(
+      ([avatar, userId]) => {
+        this.avatar = avatar;
+        this.userId = userId;
+      }
+    );
+  }
+
+  signOut() {
+    this.authService.signOut();
+  }
+}
