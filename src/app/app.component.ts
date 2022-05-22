d2e6a3effb857bab55cd13f8a52501001a9e2e39
Dylan Warren <dylanwarren19@gmail.com>
2022-05-22 00:35:10 -0500

    google Analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+
+declare const gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -6,6 +10,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor(private readonly router: Router) {
+    this.router.events
+      .pipe(
+        filter(
+          (event): event is NavigationEnd => event instanceof NavigationEnd
+        )
+      )
+      .subscribe((event) => {
+        gtag('event', 'page_view', {
+          page_path: event.urlAfterRedirects,
+        });
+      });
+  }
+
   onActivate() {
     window.scroll(0, 0);
   }
