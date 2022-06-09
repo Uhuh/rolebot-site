9351b3297e0ef5fc9333877f4122b87a785e8f55
Dylan Warren <dylanwarren19@gmail.com>
2022-06-09 15:01:12 -0500

    refactor: New global color pallete and background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { fromEvent, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   onActivate() {
     window.scroll(0, 0);
+  }
+
+  destroy$ = new Subject<void>();
+
+  constructor() {
+    // I don't like this lol
+    fromEvent(window, 'scroll').pipe(takeUntil(this.destroy$)).subscribe(() => {
+      const element = document.getElementById('nav-container')
+
+      if (!element) return;
+
+      if (window.scrollY > 5) {
+        element.classList.add('scrolled')
+      } else {
+        element.classList.remove('scrolled')
+      }
+    })
+  }
+
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
   }
 }
