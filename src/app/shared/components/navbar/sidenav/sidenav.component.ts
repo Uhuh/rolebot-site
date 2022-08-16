22011c64cc21feed7919e31ffccfc641e7319b66
Dylan Warren <dylanwarren19@gmail.com>
2022-08-15 23:34:40 -0500

    feat: Sidenav!

--- src/app/shared/components/navbar/sidenav/sidenav.component.ts
+++ src/app/shared/components/navbar/sidenav/sidenav.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { JwtService } from 'src/app/shared/services/jwtHandler.service';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
+  styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent implements OnInit {
+  isFresh = false;
+  enabled = false;
 
-  constructor() { }
+  constructor(private readonly jwtService: JwtService) {}
 
   ngOnInit(): void {
+    this.jwtService.isFresh$.subscribe((isFresh) => (this.isFresh = !!isFresh));
   }
-
 }
