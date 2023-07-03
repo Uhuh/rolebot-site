89685e04f8aae02753a4cb8f21031a47cd21b545
Dylan Warren <dylanwarren19@gmail.com>
2023-07-03 15:19:43 -0500

    update: remove unused packages and code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
-import { faBullhorn } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +7,6 @@ import { faBullhorn } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit, OnDestroy {
-  faIcon = faBullhorn;
   onActivate() {
     window.scroll(0, 0);
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
 import { NavbarModule } from './shared/components/navbar/navbar.module';
 import { FooterModule } from './shared/components/footer/footer.module';
 import { HttpClientModule } from '@angular/common/http';
@@ -12,7 +10,6 @@ import {
   NgxGoogleAnalyticsModule,
   NgxGoogleAnalyticsRouterModule,
 } from 'ngx-google-analytics';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
@@ -23,15 +20,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     NavbarModule,
     FooterModule,
     HttpClientModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production,
-      // Register the ServiceWorker as soon as the app is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000',
-    }),
     NgxGoogleAnalyticsModule.forRoot('G-J9V0P4S4H7'),
     NgxGoogleAnalyticsRouterModule,
-    FontAwesomeModule,
     BrowserAnimationsModule,
   ],
   bootstrap: [AppComponent],
